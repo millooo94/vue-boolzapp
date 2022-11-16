4fed2f4487bc338e5d2b795f9f3c644680584281
millooo94 <camillomusmeciunict@gmail.com>
2022-11-16 22:11:17 +0100

    add contents header

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const app = new Vue({
     el: '.container',
     data: {
+
+        
+        
         contacts: [
             {
             name: 'Michele',
@@ -163,9 +166,46 @@ const app = new Vue({
             }
             ],
             }
-            ]
-    },
-    methods: {
+            ],
+            selectedContact: {
+                name: 'Michele',
+                avatar: 'avatar_1.jpg',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                message: 'Hai portato a spasso il cane?',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                message: 'Ricordati di stendere i panni',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 16:15:22',
+                message: 'Tutto fatto!',
+                status: 'received'
+                }
+                ],
+                },
 
+                isReceived: null
+                
+    },
+    
+    methods: {
+        selectContact(index){
+            this.selectedContact = this.contacts[index]
+        },
+
+        isReceivedSelector(index) {
+            if(this.selectedContact.messages[index].status == 'received'){
+                this.isReceived = true
+            } else {
+                this.isReceived = false
+            }
+            return this.isReceived
+        } 
     },
 })
