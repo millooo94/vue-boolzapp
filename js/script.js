9d991b724d9d7339832fe07e71ba85c734e243f0
millooo94 <camillomusmeciunict@gmail.com>
2022-11-18 15:12:13 +0100

    add last access

--- js/script.js
+++ js/script.js
@@ -10,17 +10,20 @@ const app = new Vue({
             {
             date: '10/01/2020 15:30:55',
             message: 'Hai portato a spasso il cane?',
-            status: 'sent'
+            status: 'sent',
+            isActive:  false
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'Ricordati di stendere i panni',
-            status: 'sent'
+            status: 'sent',
+            isActive:  false
             },
             {
             date: '10/01/2020 16:15:22',
             message: 'Tutto fatto!',
-            status: 'received'
+            status: 'received',
+            isActive:  false
             }
             ],
             },
@@ -32,17 +35,20 @@ const app = new Vue({
             {
             date: '20/03/2020 16:30:00',
             message: 'Ciao come stai?',
-            status: 'sent'
+            status: 'sent',
+            isActive:  false
             },
             {
             date: '20/03/2020 16:30:55',
             message: 'Bene grazie! Stasera ci vediamo?',
-            status: 'received'
+            status: 'received',
+            isActive:  false
             },
             {
             date: '20/03/2020 16:35:00',
             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-            status: 'sent'
+            status: 'sent',
+            isActive:  false
             }
             ],
             },
@@ -54,17 +60,20 @@ const app = new Vue({
             {
             date: '28/03/2020 10:10:40',
             message: 'La Marianna va in campagna',
-            status: 'received'
+            status: 'received',
+            isActive:  false
             },
             {
             date: '28/03/2020 10:20:10',
             message: 'Sicuro di non aver sbagliato chat?',
-            status: 'sent'
+            status: 'sent',
+            isActive:  false
             },
             {
             date: '28/03/2020 16:15:22',
             message: 'Ah scusa!',
-            status: 'received'
+            status: 'received',
+            isActive:  false
             }
             ],
             },
@@ -76,12 +85,14 @@ const app = new Vue({
             {
             date: '10/01/2020 15:30:55',
             message: 'Lo sai che ha aperto una nuova pizzeria?',
-            status: 'sent'
+            status: 'sent',
+            isActive:  false
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'Si, ma preferirei andare al cinema',
-            status: 'received'
+            status: 'received',
+            isActive:  false
             }
             ],
             },
@@ -93,12 +104,14 @@ const app = new Vue({
             {
             date: '10/01/2020 15:30:55',
             message: 'Ricordati di chiamare la nonna',
-            status: 'sent'
+            status: 'sent',
+            isActive:  false
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'Va bene, stasera la sento',
-            status: 'received'
+            status: 'received',
+            isActive:  false
             }
             ],
             },
@@ -110,17 +123,20 @@ const app = new Vue({
             {
             date: '10/01/2020 15:30:55',
             message: 'Ciao Claudia, hai novità?',
-            status: 'sent'
+            status: 'sent',
+            isActive:  false
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'Non ancora',
-            status: 'received'
+            status: 'received',
+            isActive:  false
             },
             {
             date: '10/01/2020 15:51:00',
             message: 'Nessuna nuova, buona nuova',
-            status: 'sent'
+            status: 'sent',
+            isActive:  false
             }
             ],
             },
@@ -132,12 +148,14 @@ const app = new Vue({
             {
             date: '10/01/2020 15:30:55',
             message: 'Fai gli auguri a Martina che è il suo compleanno!',
-            status: 'sent'
+            status: 'sent',
+            isActive:  false
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'Grazie per avermelo ricordato, le scrivo subito!',
-            status: 'received'
+            status: 'received',
+            isActive:  false
             }
             ],
             },
@@ -149,17 +167,20 @@ const app = new Vue({
             {
             date: '10/01/2020 15:30:55',
             message: 'Ciao, andiamo a mangiare la pizza stasera?',
-            status: 'received'
+            status: 'received',
+            isActive:  false
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-            status: 'sent'
+            status: 'sent',
+            isActive:  false
             },
             {
             date: '10/01/2020 15:51:00',
             message: 'OK!!',
-            status: 'received'
+            status: 'received',
+            isActive:  false
             }
             ],
             }
@@ -172,26 +193,28 @@ const app = new Vue({
                 {
                 date: '10/01/2020 15:30:55',
                 message: 'Hai portato a spasso il cane?',
-                status: 'sent'
+                status: 'sent',
+                isActive:  false
                 },
                 {
                 date: '10/01/2020 15:50:00',
                 message: 'Ricordati di stendere i panni',
-                status: 'sent'
+                status: 'sent',
+                isActive:  false
                 },
                 {
                 date: '10/01/2020 16:15:22',
                 message: 'Tutto fatto!',
-                status: 'received'
+                status: 'received',
+                isActive:  false
                 }
                 ],
                 },
                 searchContacts: '',
-                newMessageSentMessage: '',
-                newMessageSentDate: '',
-                newMessageReceivedMessage: 'Ok',
-                newMessageReceivedDate: '',
-                isActive: false       
+                newMessage: '',
+                lastAccess: 'Ultimo accesso'
+
+                      
                 
     },
     
@@ -200,53 +223,49 @@ const app = new Vue({
             this.selectedContact = this.contacts[index]
         },
 
-        addMessage() {
-            if (this.newMessageSentMessage.trim()) {
-                this.selectedContact.messages.push({date: this.currentSentDate(), message: this.newMessageSentMessage.trim(), status: 'sent'}),
-                this.newMessageSentMessage = '';
-               }
-           },
-        
-        
-        answerMessage() {
-            setTimeout(() => this.selectedContact.messages.push({date: this.currentReceivedDate(), message: this.newMessageReceivedMessage, status: 'received'}), 2000);
-            this.newMessageReceivedDate = '';
-        },
-
-
         includesCharacters(contact) {
             console.log('test search:', this.searchContacts);
             return contact.name.includes(this.searchContacts)
         },
 
-        currentReceivedDate() {
-            const date = new Date
-            this.newMessageReceivedDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-            this.newMessageReceivedDate = this.newMessageReceivedDate.slice(11, 16)
-            return this.newMessageReceivedDate
+        changeMenuIndex(index){
+            this.selectedContact.messages[index].isActive = !this.selectedContact.messages[index].isActive
         },
-        
-        currentSentDate() {
-            const date = new Date
-            this.newMessageSentDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-            this.newMessageSentDate = this.newMessageSentDate.slice(11, 16)
-            return this.newMessageSentDate
-        },
-        changeMenuIndex(){
-            this.isActive = !this.isActive
-        },
+
         deleteMessage(index){
-          this.selectedContact.messages.splice(index, 1)
+           this.selectedContact.messages.splice(index, 1)
         },
-        okok(i){
-            if(selectedContact.messages[i] == true) {
-                isActive = true
+
+        sendMessage() {
+            if(this.newMessage != ''){
+                this.selectedContact.messages.push({
+                    date: this.getNow(),
+                    message: this.newMessage,
+                    status: 'sent',
+                    isActive: false
+                });
+                this.newMessage = '';
+                this.lastAccess = 'Sta scrivendo...'
+                let receiver = this.selectedContact
+                setTimeout(() => {
+                    receiver.messages.push({
+                        date: this.getNow(),
+                        message: 'Ok',
+                        status: 'received',
+                        isActive: false
+                    })
+                    this.lastAccess = `Ultimo accesso oggi alle ${this.selectedContact.messages[this.selectedContact.messages.length - 1].date.slice(11,16)} `
+                    ;
+                }, 5000);   
             }
-        }
+		},
+		getNow() {
+			return luxon.DateTime.now().toFormat('dd/MM/yyyy HH:mm:ss');
+		}
+    },
 
 
-
-    }
+    
 })
 
 
